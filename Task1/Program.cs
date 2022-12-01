f6bfc5d9e03be6da83bde4235a9fea0914fa1c57
ANDREI <ugryumovandrej@yandex.ru>
2022-12-01 16:38:36 +0300

    DZ done

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿//  цикл  , где мы вводим 2 числа , второе - степень первого.
+
+int n1  = 1;
+int n2  = 1;
+while(n1 != 0 && n2 != 0)
+{    
+    Console.WriteLine("Введите число и степень; если хотите прервать программу введите оба числа нули");
+    n1 = Convert.ToInt32(Console.ReadLine());
+    n2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{n1}^{n2}={Math.Pow(n1, n2)}");
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,69 @@
+﻿// *Немного усложнил задачу 
+// Создаем автозавполняемый рандомными числами массив, и потом второй который заполняется только макс и мин значениями
+// из первого массива, при чем в рандомной последовательности.
+ void FillArray(int[]massive)
+ {
+    int dlm = massive.Length;
+    int i = 0;
+    while (i<dlm)
+    {
+        massive[i] = new Random().Next(0,1000);
+        i++;
+    }
+ }
+void PrintArray(int[]massivchik)
+{
+    int dlina = massivchik.Length;
+    int index = 0;
+    while(index<dlina)
+    {
+        Console.WriteLine(massivchik[index]);
+        index++;
+    }
+}
+
+int[]array = new int[20];
+
+FillArray(array);
+
+int maxim = -1;
+int minim = 1000;
+int c = 0;
+int len = array.Length;
+while(c<len)
+{
+    if(array[c]<minim)
+    {
+        minim = array[c];
+        c++;
+    }
+    else if(array[c]>maxim)
+    {    maxim = array[c];
+         c++;
+    }     
+    else
+    {
+        c++;
+    }    
+}
+
+int[]array2 = new int[10];
+int index = 0;
+int result = new Random().Next(0,2);
+while(index<10)
+{
+    //array2[index] = maxim || minim;
+    if(result == 0)
+        array2[index] = maxim;
+    else
+    {
+        array2[index] = minim;
+    }
+    index++;
+    result = new Random().Next(0,2);
+}
+
+Console.WriteLine(minim);
+Console.WriteLine(maxim);
+
+PrintArray(array2);
